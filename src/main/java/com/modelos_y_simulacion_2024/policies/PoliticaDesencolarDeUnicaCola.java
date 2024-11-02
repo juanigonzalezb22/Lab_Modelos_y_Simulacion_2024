29a1e32754495a0a35d4647376b7ac43fe391e5c
dglabella <dglab89@gmail.com>
2024-11-02 14:11:17 -0300

    saving progress

--- src/main/java/com/modelos_y_simulacion_2024/policies/PoliticaDesencolarDeUnicaCola.java
+++ src/main/java/com/modelos_y_simulacion_2024/policies/PoliticaDesencolarDeUnicaCola.java
@@ -6,8 +6,8 @@ import java.util.Collection;
 import com.modelos_y_simulacion_2024.dominio.Entidad;
 import com.modelos_y_simulacion_2024.dominio.Queue;
 
-public class PoliticaDesencolarDeUnicaCola implements SelectionPolicy<Queue, Entidad>{
-
+public class PoliticaDesencolarDeUnicaCola implements SelectionPolicy<Queue, Entidad> {
+    //Modelar bien que informacion es necesario para poder implementar deque policies
     @Override
     public Entidad select(Collection<Queue> options) {
         Queue queue = null;
